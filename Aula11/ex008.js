749a49f09e1cc89835975208bd45bbf4467d896b
Felipe Marinho <felipe.pinto.marinho@gmail.com>
2023-08-19 16:23:31 -0300

    Códigos em JS e HTML com estruturas condicionais.

---
+++ Aula11/ex008.js
@@ -0,0 +1,22 @@
+console.log('O console funcionou corretamente!')
+var vel = 60.5
+console.log(`A velocidade do seu carro é: ${vel} Km/h`)
+
+//Estrutura condicional
+if(vel > 80){
+    console.log('Sua velocidade está acima do limite. MULTADO!')
+}
+else{
+    console.log('Sua velocidade está abaixo do limite premitida. PARABÉNS!')
+}
+
+console.log('Dirija sempre com segurança.')
+
+var pais = 'EUA'
+console.log(`Vivendo em ${pais}`)
+if (pais != 'Brasil'){
+    console.log('Você é estrangeiro.')
+}
+else{
+    console.log('Você é brasileiro.')
+}
