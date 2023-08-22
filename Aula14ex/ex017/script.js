1d9790a54bebe5ab7f4c7eb3631f2aed1dce23ed
Felipe Marinho <felipe.pinto.marinho@gmail.com>
2023-08-22 12:32:41 -0300

    Adicionada a super tabuada

--- Aula14ex/ex017/script.js
+++ Aula14ex/ex017/script.js
@@ -1,1 +1,20 @@
-alert('Olá!')
+function tabuada(){
+    let numero = window.document.getElementById('txtnum')
+    let tab = window.document.getElementById('seltab')
+    
+    if(numero.value.length == 0){
+        alert('ERRO -- VALORES FALTANTES')
+    }
+    else{
+        let n = Number(numero.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+}
